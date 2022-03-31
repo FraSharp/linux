82e14d9b5a7c139c82fc1e0f4b5e88a0ae66d44d
FraSharp <s23265@iisve.it>
2022-03-31 20:04:28 +0200

    build script: cleanup to reflect latest tc/configs changes
    Signed-off-by: FraSharp <s23265@iisve.it>

--- regen.sh
+++ regen.sh
@@ -1,2 +1,4 @@
-make LLVM=1 LLVM_IAS=1 arch_lsmod_defconfig -j69
-cp .config arch/x86/configs/arch_lsmod_defconfig
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
+make LLVM=1 LLVM_IAS=1 arch_defconfig -j69
+
+cp .config arch/x86/configs/arch_defconfig
