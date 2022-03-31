aae3b2b2668019b0bbf1a3d3cf48a1bab899af61
FraSharp <s23265@iisve.it>
2022-03-31 20:04:28 +0200

    build script: cleanup to reflect latest tc/configs changes
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 export ARCH=x86
 
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
 make arch_defconfig LLVM=1 LLVM_IAS=1 -j69
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
 make LLVM=1 LLVM_IAS=1 -j69
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
 make modules LLVM=1 LLVM_IAS=1 -j69
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
 sudo make modules_install LLVM=1 LLVM_IAS=1 -j69
+PATH="$HOME/tc/clang-14/bin:${PATH}" \
 make bzImage LLVM=1 LLVM_IAS=1 -j69
+
 sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-linux517
 sudo mkinitcpio -p linux517
 sudo grub-mkconfig -o /boot/grub/grub.cfg
