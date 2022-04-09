b4769b7275e9a9cb594b0bdaa740cd36755e2de1
FraSharp <s23265@iisve.it>
2022-04-10 00:10:58 +0200

    mm/memory_failure: trace_memory_failure_event only if CONFIG_RAS is defined
    Signed-off-by: FraSharp <s23265@iisve.it>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1136,7 +1136,9 @@ static struct page_state error_states[] = {
 static void action_result(unsigned long pfn, enum mf_action_page_type type,
 			  enum mf_result result)
 {
+#ifdef CONFIG_RAS
 	trace_memory_failure_event(pfn, type, result);
+#endif
 
 	pr_err("Memory failure: %#lx: recovery action for %s: %s\n",
 		pfn, action_page_types[type], action_name[result]);
